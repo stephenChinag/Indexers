5c60ce205d37b120806595b209374028d456da7a
stephenChinag <stephenchinag@gmail.com>
2025-03-14 13:22:21 +0100

    setting

--- obj/Debug/net8.0/Indexers.AssemblyInfo.cs
+++ obj/Debug/net8.0/Indexers.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Indexers")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9a8fd5a62f8112ee130d6fa44cfc7696121d358")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+206fc70357100c6b13c2f430879257faba30af63")]
 [assembly: System.Reflection.AssemblyProductAttribute("Indexers")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Indexers")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
